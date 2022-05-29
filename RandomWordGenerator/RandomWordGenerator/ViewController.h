f888d13d520181827208a6bb662e0dcf77965260
jamesbalsa61 <81057352+jamesbalsa61@users.noreply.github.com>
2022-05-29 12:08:14 -0700

    Add files via upload

---
+++ RandomWordGenerator/RandomWordGenerator/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  RandomWordGenerator
+//
+//  Created by Michael Balsa on 5/29/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UILabel *label;
+
+- (IBAction)randomNumber:(id)sender;
+
+@end
+
