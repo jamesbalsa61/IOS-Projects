83edc18326c5af5fd1551d35d16ba6efb287742b
jamesbalsa61 <81057352+jamesbalsa61@users.noreply.github.com>
2022-06-13 15:22:01 -0700

    Add files via upload

---
+++ Calculator/Calculator/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  Calculator
+//
+//  Created by Michael Balsa on 6/12/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
