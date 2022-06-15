3521022fb8757cc9f7fcf09813102b6274722e39
jamesbalsa61 <81057352+jamesbalsa61@users.noreply.github.com>
2022-06-14 17:24:20 -0700

    Add files via upload

---
+++ Temperature/Temperature/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Temperature
+//
+//  Created by Michael Balsa on 6/13/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
